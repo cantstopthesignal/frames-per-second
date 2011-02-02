1ddef64a5defde139d3bee77d0201964565c3724
Griff Hazen <griffhazen@gmail.com>
2011-02-02 10:34:29 -0800

    Initial commit: base appengine project

---
+++ src/com/cantstopthesignals/FramesPerSecondServlet.java
@@ -0,0 +1,13 @@
+package com.cantstopthesignals;
+
+import java.io.IOException;
+import javax.servlet.http.*;
+
+@SuppressWarnings("serial")
+public class FramesPerSecondServlet extends HttpServlet {
+  public void doGet(HttpServletRequest req, HttpServletResponse resp)
+      throws IOException {
+    resp.setContentType("text/plain");
+    resp.getWriter().println("Hello, world");
+  }
+}
